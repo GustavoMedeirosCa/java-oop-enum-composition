1126bda6d5f824020c80d95f736ea0598a868167
Gustavo Medeiros <gustavomedeiros.c451@gmail.com>
2025-12-08 09:44:36 -0300

    feat: add totalValue method to HourContract

--- src/entities/HourContract.java
+++ src/entities/HourContract.java
@@ -39,4 +39,8 @@ public class HourContract {
     public void setHours(Integer hours) {
         this.hours = hours;
     }
+
+    public double totalvalue() {
+        return valuePerHour * hours;
+    }
 }
